8ec9ff45859f01f0db5e6de59a467ff52abc3d38
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-20 20:13:45 -0400

    using wsdl to create the course endpoints

--- soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,15 +1,21 @@
 package com.prettylegit.soapcoursemanagement.soap;
 
-import net.jimmywin.courses.CourseDetails;
-import net.jimmywin.courses.GetCourseDetailsRequest;
-import net.jimmywin.courses.GetCourseDetailsResponse;
+import com.prettylegit.soapcoursemanagement.soap.bean.Course;
+import com.prettylegit.soapcoursemanagement.soap.service.CourseDetailsService;
+import net.jimmywin.courses.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.List;
+
 @Endpoint
 public class CourseDetailsEndpoint {
+
+    @Autowired
+    CourseDetailsService service;
     //method
     // input - GetCourseDetailsRequest
     // output - GetCourseDetailsResponse
@@ -18,12 +24,42 @@ public class CourseDetailsEndpoint {
     @PayloadRoot(namespace = "http://jimmywin.net/courses", localPart = "GetCourseDetailsRequest")
     @ResponsePayload
     public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        Course course = service.findById(request.getId());
+
+        return mapCourseDetails(course);
+    }
+
+    @PayloadRoot(namespace = "http://jimmywin.net/courses", localPart = "GetAllCourseDetailsRequest")
+    @ResponsePayload
+    public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
+        List<Course> courses = service.findAll();
+
+        return mapAllCourseDetails(courses);
+    }
+
+    private GetCourseDetailsResponse mapCourseDetails(Course course) {
         GetCourseDetailsResponse response = new GetCourseDetailsResponse();
-        CourseDetails courseDetais = new CourseDetails();
-        courseDetais.setId(request.getId());
-        courseDetais.setName("Microservices Course");
-        courseDetais.setDescription("This course will help you to understand Microservices");
-        response.setCourseDetails(courseDetais);
+        response.setCourseDetails(mapCourse(course));
         return response;
+    }
+
+    private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+        GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+        for(Course course:courses){
+            CourseDetails mapCourse = mapCourse(course);
+            response.getCourseDetails().add(mapCourse);
+        }
+        return response;
+    }
+
+    private static CourseDetails mapCourse(Course course) {
+        CourseDetails courseDetails = new CourseDetails();
+
+        courseDetails.setId(course.getId());
+
+        courseDetails.setName(course.getName());
+
+        courseDetails.setDescription(course.getDescription());
+        return courseDetails;
     }
 }
--- soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/WebServiceConfig.java
+++ soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/WebServiceConfig.java
@@ -4,9 +4,13 @@ import jakarta.servlet.ServletRegistration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 //Enable Spring Web Services
 @EnableWs
@@ -18,10 +22,30 @@ public class WebServiceConfig {
     // url -> /ws/*
 
     @Bean
-    public ServletRegistration messageDispatcherServlet(ApplicationContext context) {
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
         MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
         messageDispatcherServlet.setApplicationContext(context);
         messageDispatcherServlet.setTransformWsdlLocations(true);
-        return (ServletRegistration) new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+        return new ServletRegistrationBean<>(messageDispatcherServlet, "/ws/*");
+    }
+    // /ws/courses.wsdl
+     // CoursePort
+    //
+
+    @Bean(name = "courses")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+
+        definition.setPortTypeName("CoursePort");
+        definition.setTargetNamespace("http://jimmywin.net/courses");
+        definition.setLocationUri("/ws");
+        definition.setSchema(coursesSchema);
+
+        return definition;
+    }
+
+    @Bean
+    public XsdSchema coursesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
     }
 }
