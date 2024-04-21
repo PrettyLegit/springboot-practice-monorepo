a3a03eff8098bfcb09b27fdfe63187f9b5132c9d
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-21 19:53:04 -0400

    implementing exception handling 404 not found

--- restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserResource.java
@@ -23,7 +23,13 @@ public class UserResource {
 
     @GetMapping(path = "/users/{id}")
     public User getUserById(@PathVariable Integer id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException("id: " + id);
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
