3f1df546e6dad2cb567c5980da5b2327ef07d2a8
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-21 17:10:17 -0400

    NOT WORKING YET implementing security for SOAP web services with WS Security

--- soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/WebServiceConfig.java
+++ soap-course-management/src/main/java/com/prettylegit/soapcoursemanagement/soap/WebServiceConfig.java
@@ -7,16 +7,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import java.util.Collections;
+import java.util.List;
+
 //Enable Spring Web Services
 @EnableWs
 // Spring Configuration
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
     // MessageDispatcherServlet
     // ApplicationContext
     // url -> /ws/*
@@ -48,4 +55,28 @@ public class WebServiceConfig {
     public XsdSchema coursesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
     }
+
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setSecurementActions("UsernameToken");
+        securityInterceptor.setValidationCallbackHandler(callbackHandler());
+
+        return securityInterceptor;
+    }
+
+    @Bean
+    public SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+        handler.setUsersMap(Collections.singletonMap("user", "password"));
+        return handler;
+    }
+
+    // Interceptors.add -> XwsSecurityInterceptor
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
+
+
 }
