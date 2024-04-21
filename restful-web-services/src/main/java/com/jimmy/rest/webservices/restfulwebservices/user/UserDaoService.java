ba859ead17a95b1fe1e0fb55657ad5e5a08a08c5
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-21 19:42:51 -0400

    enhancing POST method to return correct HTTP Status Code and Location

--- restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -13,11 +13,12 @@ import java.util.Objects;
 public class UserDaoService {
 
      private static List<User> users = new ArrayList<>();
+     private static int usersCount = 0;
 
      static {
-         users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
-         users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
-         users.add(new User(3,"Jim", LocalDate.now().minusYears(20)));
+         users.add(new User(++usersCount,"Adam", LocalDate.now().minusYears(30)));
+         users.add(new User(++usersCount,"Eve", LocalDate.now().minusYears(25)));
+         users.add(new User(++usersCount,"Jim", LocalDate.now().minusYears(20)));
      }
      public List<User> findAll(){
          return users;
@@ -32,5 +33,9 @@ public class UserDaoService {
          return null;
      }
 
-
+     public User save(User user){
+         user.setId(++usersCount);
+         users.add(user);
+         return user;
+     }
 }
--- restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.jimmy.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,4 +26,13 @@ public class UserResource {
         return service.findOne(id);
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
 }
