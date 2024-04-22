83ce439fdb4f04da09e201e3de2110e1421707a2
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-22 00:46:44 -0400

    Explored internationalization for REST API

--- restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,20 @@
 package com.jimmy.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -22,4 +33,12 @@ public class HelloWorldController {
                 String.format("Hello world, %s", name)
         );
     }
+
+    @GetMapping(path = "/hello-world-i11n")
+    public String helloWorldI11n(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//        return "Hello World V2";
+    }
+
 }
