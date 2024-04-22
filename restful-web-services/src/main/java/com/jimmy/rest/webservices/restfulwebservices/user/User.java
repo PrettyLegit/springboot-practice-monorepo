ff67cb21557d1195c95422eba5dbdbdced792939
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-04-21 23:27:52 -0400

    implemented validations for REST API

--- restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/jimmy/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,15 @@
 package com.jimmy.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
     private Integer id;
+    @Size(min = 2, message = "Name should have atleast 2 characters")
     private String name;
+    @Past(message = "Birthdate should be in the past")
     private LocalDate birthdate;
 
     public User(Integer id, String name, LocalDate birthdate) {
